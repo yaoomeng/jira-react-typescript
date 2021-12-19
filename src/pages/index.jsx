2dbf4dcc1460d270fa1013b567c1107bed7828be
yurenguo <2630445979@qq.com>
2021-12-19 10:07:37 +0800

    项目初始化

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React, { memo } from 'react'
+
+export default memo(function index() {
+    const [state, setstate] = useState(initialState)
+    return (
+        <div>
+
+        </div>
+    )
+})
